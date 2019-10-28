c69676e92040f99936c59b8681b60b700d0b93d3
perXautomatik <christoffer.broback@gmail.com>
2019-10-28 14:47:50 +0100

    reorganization

--- görBackUpPåSweref.bat
+++
@@ -1,9 +0,0 @@
-cd /D "G:\sbf\Livsmilj”\Milj”- och h„lsoskydd\Vatten\Avlopp\Klart Vatten\Fastigheter\GIS"
-mkdir "Arbetsskikt\Annat\backup\FlaggbackupFastigheterSweref\batchBackup\%date:~%"
-xcopy "Fastigheter_Sweref.*" "Arbetsskikt\Annat\backup\FlaggbackupFastigheterSweref\batchBackup\%date:~%" /c /r /v /y > "Arbetsskikt\Annat\backup\FlaggbackupFastigheterSweref\batchBackup\%date:~%\xcopy.log" 
-
-cd /D "C:\Program Files\MapInfo\Professional"
-
-start MapInfoPro.exe
-
-exit
