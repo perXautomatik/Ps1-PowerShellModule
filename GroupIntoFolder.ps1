e48ef8e7cd878f383d0ac14697e2a544776698e3
perXautomatik <christoffer.broback@gmail.com>
2019-10-30 16:19:19 +0100

    Update GroupIntoFolder.ps1

--- GroupIntoFolder.ps1
+++ GroupIntoFolder.ps1
@@ -1,8 +1,10 @@
 #https://stackoverflow.com/questions/41467996/powershell-create-a-folder-from-a-file-name-then-place-that-file-in-the-folde
 
-$path = 'C:\Users\crbk01\OneDrive - Region Gotland\Till Github\PowerShell and bash'
-
- dir $path\*.* | Group-Object -Property extension -AsHashTable 
-
-   # Get files
- # Group-Object {$_.fullname} |  # Group by part before first underscore
+Get-ChildItem -File |  # Get files
+  Group-Object { $_.Name -replace '[(]?\d*[)]?$' } |  # Group by part before first underscore
+  ForEach-Object {
+    # Create directory
+    $dir = New-Item -Type Directory -Name $_.Name
+    # Move files there
+    $_.Group | Move-Item -Destination $dir
+  }
