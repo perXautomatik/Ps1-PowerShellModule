1ecaeed56eebae7358f2e8942c78fe58d03036c6
perXautomatik <christoffer.broback@gmail.com>
2020-01-06 21:30:22 +0100

    Update GroupIntoFolder.ps1

--- GroupIntoFolder.ps1
+++ GroupIntoFolder.ps1
@@ -1,7 +1,7 @@
 #https://stackoverflow.com/questions/41467996/powershell-create-a-folder-from-a-file-name-then-place-that-file-in-the-folde
 
 Get-ChildItem -File |  # Get files
-  Group-Object { $_.Name -replace '[(]?\d*[)]?$' } |  # Group by part before first underscore
+  Group-Object { $_.Name -replace '[.][^.]*$' } |  #[(]?\d*[)]?$ folders by nameExceptDigit
   ForEach-Object {
     # Create directory
     $dir = New-Item -Type Directory -Name $_.Name
