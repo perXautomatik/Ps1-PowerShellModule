313aecc299d3906b740210b65d67a14b72a5fad2
perXautomatik <christoffer.broback@gmail.com>
2020-03-30 17:05:52 +0200

    Join with Vscode

--- GroupIntoFolderByExtension.ps1
+++ GroupIntoFolderByExtension.ps1
@@ -1,7 +1,7 @@
 #https://stackoverflow.com/questions/41467996/powershell-create-a-folder-from-a-file-name-then-place-that-file-in-the-folde
 
 Get-ChildItem -File |  # Get files
-  Group-Object { $_.Name -replace '^[^.]*[.]' } |  # Group by part before first underscore
+  Group-Object { $_.Name -replace '[.][^.]*$' } |  # Group by part before first underscore
   ForEach-Object {
     # Create directory
     $dir = New-Item -Type Directory -Name $_.Name
