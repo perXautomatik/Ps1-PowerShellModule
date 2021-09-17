ccd508a4e23565b2c813fc450e72cc32dc448ecc
perXautomatik <christoffer.broback@gmail.com>
2021-09-17 16:15:34 +0200

    can't access the root of the tree

--- actOnJson.ps1
+++ actOnJson.ps1
@@ -2,7 +2,7 @@
 . .\getChildrenRecursive.ps1
 . .\presentJson.ps1
 
-$jsonx = "D:\OneDrive\TabSessionManager - Backup\BookmarkCommanderExport.txt"
+$jsonx = "D:\OneDrive\TabSessionManager - Backup\tablerone_backup_with_thumbs_2021-04-19-09-33-16.txt"
 $jsonx = Get-Content $jsonx -Raw | ConvertFrom-Json
 
 $presentationMethodPath = (Join-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent) -ChildPath "presentJson.ps1" )
--- getChildrenRecursive.ps1
+++ getChildrenRecursive.ps1
@@ -7,17 +7,12 @@ function getChildrenRecursive {
 
       if($depth -eq $null) {$depth = 0}
       $str = '-' * $depth;        
-            
-    foreach ($item in $object.PSObject.Properties) {
-        if ($item.TypeNameOfValue -eq 'System.Object') {    
-            if($item.Value.children) {
-                $depth = 1+$depth;
-                getChildrenRecursive $item.Value.children $pMethodPath $depth;
-            }
-            else {
-                $item
-            }    
-        }
-    }    
-    $object
+
+      $q = $object.Value
+
+      foreach ($item in $q)
+      {
+          $item
+      }
+
 }
