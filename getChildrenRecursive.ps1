ddec72d22d1b6be45715e6ce03835e20beabaaf9
perXautomatik <christoffer.broback@gmail.com>
2021-09-17 15:26:08 +0200

    suddenly don't parse each entry indevidually

--- getChildrenRecursive.ps1
+++ getChildrenRecursive.ps1
@@ -5,32 +5,19 @@ function getChildrenRecursive {
         [Parameter(Mandatory=$false)][int32] $depth
       )
 
-        
-      
-        'presenting' 
-        $json = [ordered]@{}
-        
-        ($object).PSObject.Properties |
-            ForEach-Object { $json[$_.Name] = $_.Value }
-        
-        $json.SyncRoot
-
       if($depth -eq $null) {$depth = 0}
-      
       $str = '-' * $depth
-      
+            
     foreach ($item in $object.PSObject.Properties) {
             if ($item.TypeNameOfValue -eq 'System.Object') {    
                 if($item.Value.children) {
-                $depth = 1+$depth                                                                                    
-                getChildrenRecursive $item.Value.children $pMethodPath $depth   
+                 $depth = 1+$depth                                                                                    
+                    getChildrenRecursive $item.Value.children $pMethodPath $depth   
                 }
             }
             else {
-                $json
+                write-Host "$str DEPTH  $depth " + $object.title + "`n"
             }           
         }
-        
-
- 
+         
 }
