7f2a9847d190a0cdc229f39d610bc00a78ade4c6
perXautomatik <christoffer.broback@gmail.com>
2022-02-08 11:41:32 +0100

    after merge

---
+++ Microsoft.PowerShellISE_profile.ps1
@@ -0,0 +1,11 @@
+
+
+#Module Browser Begin
+#Version: 1.0.0
+Add-Type -Path 'E:\Program Files (x86)\Microsoft Module Browser\ModuleBrowser.dll'
+$moduleBrowser = $psISE.CurrentPowerShellTab.VerticalAddOnTools.Add('Module Browser', [ModuleBrowser.Views.MainView], $true)
+$psISE.CurrentPowerShellTab.VisibleVerticalAddOnTools.SelectedAddOnTool = $moduleBrowser
+#Module Browser End
+# Start AzureAutomationISEAddOn snippet
+Import-Module AzureAutomationAuthoringToolkit
+# End AzureAutomationISEAddOn snippet
---
+++ link to app launcher.md
@@ -0,0 +1,2 @@
+﻿link to app launcher
+store aplauncher locally, link it's apfolder 
