3c62b66cbbf58bdeebfcdf8a7f41b663386ba85c
perXautomatik <christoffer.broback@gmail.com>
2022-03-09 14:45:09 +0100

    [feature] git configuration

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -39,6 +39,9 @@ Set-PSReadlineKeyHandler -Chord Tab -Function MenuComplete
 #ps ExecutionPolicy;
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 
+#https://stackoverflow.com/questions/47356782/powershell-capture-git-output
+#Then stderr should be redirected to stdout.
+set GIT_REDIRECT_STDERR=2>&1
 
 #------------------------------- Set Paths           -------------------------------
 
@@ -53,13 +56,26 @@ set-PSReadlineOption -HistorySavePath "D:\AppData\Roaming\Microsoft\Windows\Powe
 #path to list of aps to install
 #choco ask to install if not present
 
-#list of portable apps,download source 
+#list of portable apps,download source
 #path
 #download and extract if not present, ask to confirm
 
-#git Repos paths and origions,
+#path to portable apps
+#path to standard download location
 
-#------------------------------- Set Paths  end       -------------------------------
+
+#git Repos paths and origions,
+#git systemwide profile folder
+#git global path
+
+#everything data folder
+#autohotkey script to run on startup
+
+#startup programs
+
+#reg to add if not present
+
+#------------------------------- SystemMigration end  -------------------------------
 
 
 
