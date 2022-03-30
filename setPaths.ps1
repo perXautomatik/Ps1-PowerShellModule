83ab00e581159c29d95b286e61ab7a6901aabfe2
perXautomatik <christoffer.broback@gmail.com>
2022-03-30 16:45:50 +0200

    abc

--- setPaths.ps1
+++ setPaths.ps1
@@ -1,10 +1,13 @@
 
 #ps setHistorySavePath
 
-# Load scripts from the following locations
-#$env:Path += ";D:\SysAdmin\scripts\PowerShellBasics"
-#$env:Path += ";D:\SysAdmin\scripts\Connectors"
-#$env:Path += ";D:\SysAdmin\scripts\Office365"
+# Load scripts from the following locations   
+
+$profileFolder = (split-path $profile -Parent)
+$EnvPath = join-path -Path $profileFolder -ChildPath 'Snipps'
+echo "EnvPath: $EnvPath"
+
+#$env:Path += ";$EnvPath"
 
 $historyPath = "C:\Users\crbk01\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt"
 set-PSReadlineOption -HistorySavePath $historyPath
@@ -13,14 +16,14 @@ echo "historyPath: $historyPath"
 $path = [Environment]::GetEnvironmentVariable('PSModulePath', 'Machine')
 
 # vscode Portable Path
-$path = [Environment]::GetEnvironmentVariable('PSModulePath', 'Machine')
-$newpath = 'D:\portapps\6, Text,programming, x Editing\PortableApps\vscode-portable\vscode-portable.exe'
-[Environment]::SetEnvironmentVariable("code", $newpath)
+$vscodepath = 'D:\portapps\6, Text,programming, x Editing\PortableApps\vscode-portable\vscode-portable.exe'
+[Environment]::SetEnvironmentVariable("code", $vscodepath)
 
 #sqlite dll
-$workpath = "C:\Users\crbk01\AppData\Local\GMap.NET\DllCache\SQLite_v103_NET4_x64\System.Data.SQLite.DLL"
-$homepath = (everything 'System.Data.SQLite.DLL' | select -first 1)
-Add-Type -Path if(check-path $workpath){$workpath} else {$homepath}
+$workpath = "C:\Users\crbk01\AppData\Local\GMap.NET\DllCache\SQLite_v103_NET4_x64\System.Data.SQLite.DLL"  ; 
+$alternative = (everything 'System.Data.SQLite.DLL' | select -first 1) ;
+ $p = if(check-path $workpath){$workpath} else {$alternative} ;
+Add-Type -Path $p
 
 ### global variables
 $whatPulseQuery = "select rightstr(path,instr(reverse(path),'/')-1) exe,path from (select max(path) path,max(cast(replace(version,'.','') as integer)) version from applications group by case when online_app_id = 0 then name else online_app_id end)"
