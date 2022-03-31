5995a6ae3867a29239a796558844893d3a4d68bd
perXautomatik <christoffer.broback@gmail.com>
2022-03-31 18:25:20 +0200

    [checkpoint] before breaking out into separate

--- profile.ps1
+++ profile.ps1
@@ -2,95 +2,118 @@
 
 #######################################################
 
+function find-file { #todo: breakout
 
-function find-file($name) { #todo: breakout
+	param ( $name = '.' )
 	ls -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | foreach {
 		$place_path = $_.directory
 		echo "${place_path}\${_}"
 	}
 }
-function unzip ($file) { #todo: breakout
-	$dirname = (Get-Item $file).Basename
-	echo("Extracting", $file, "to", $dirname)
-	New-Item -Force -ItemType directory -Path $dirname
-	expand-archive $file -OutputPath $dirname -ShowProgress
-}
-Function aliasEFunc {Search-Everything -PathExclude 'C:\users\Crbk01\AppData\Local\Temp'-Filter '<wholefilename:child:.git file:>|<wholefilename:child:.git folder:>' -global | #todo: rename #todo: breakout
-Where{ $_ -notmatch 
-	'C..9dfe73ef|OneDrive|GitHubDesktop.app|Microsoft VS Code._.resources.app|Installer.resources.app.node_modules|Microsoft.E dge.User Data.*.Extensions|Program Files.*.(Esri|MapInfo|ArcGIS)|Recycle.Bin' 
-}}  ;
-
 Function aliasEGSRfunc #todo: breakout
 {
 	out-null -InputObject( git remote -v | Tee-Object -Variable proc ) ;
 	%{$proc -split '\n'} | %{ $properties = $_ -split '[\t\s]';
 	$remote = try{ New-Object PSObject -Property @{ name = $properties[0].Trim();
-	    url = $properties[1].Trim();  type = $properties[2].Trim() } } catch {'noRemote'} ;
+		url = $properties[1].Trim();  type = $properties[2].Trim() } } catch {'noRemote'} ;
 		$remote | select-object -first 1 | select url}
-} ;
+	} ;
 	
-# todo: breake out to file
-function sed-recursive($filePattern, $find, $replace) {
-	$files = ls . "$filePattern" -rec
-	foreach ($file in $files) {
-		(Get-Content $file.PSPath) |
-		Foreach-Object { $_ -replace "$find", "$replace" } |
-		Set-Content $file.PSPath
-	}
-}
-	
-#todo: breakout
-function sudo {
+	#todo: breakout
+	function sudo {
 	$file, [string]$arguments = $args;
 	$psi = new-object System.Diagnostics.ProcessStartInfo $file;
 	$psi.Arguments = $arguments;
 	$psi.Verb = "runas";
 	$psi.WorkingDirectory = get-location;
 	[System.Diagnostics.Process]::Start($psi) >> $null
+	}
+	
+	#todo: breakout
+	function pstree {
+	# https://gist.github.com/aroben/5542538
+		$ProcessesById = @{}
+		foreach ($Process in (Get-WMIObject -Class Win32_Process)) {
+			$ProcessesById[$Process.ProcessId] = $Process
+		}
+		
+		$ProcessesWithoutParents = @()
+		$ProcessesByParent = @{}
+		foreach ($Pair in $ProcessesById.GetEnumerator()) 
+		{
+			$Process = $Pair.Value
+		
+			if (($Process.ParentProcessId -eq 0) -or !$ProcessesById.ContainsKey($Process.ParentProcessId)) {
+				$ProcessesWithoutParents += $Process
+				continue
+			}
+		
+			if (!$ProcessesByParent.ContainsKey($Process.ParentProcessId)) {
+				$ProcessesByParent[$Process.ParentProcessId] = @()
+			}
+			$Siblings = $ProcessesByParent[$Process.ParentProcessId]
+			$Siblings += $Process
+			$ProcessesByParent[$Process.ParentProcessId] = $Siblings
+		}
+	}
+	
+	#todo: breakout
+	function Update-Packages {
+		# Python 直接执行
+		#$env:PATHEXT += ";.py"
+		# 更新系统组件
+		# update pip
+		# Write-Host "Step 1: 更新 pip" -ForegroundColor Magenta -BackgroundColor Cyan
+		# $a = pip list --outdated
+		# $num_package = $a.Length - 2
+		# for ($i = 0; $i -lt $num_package; $i++) {
+			# 	$tmp = ($a[2 + $i].Split(" "))[0]
+			# 	pip install -U $tmp
+	# }
+	
+	# update conda
+	Write-Host "Step 1: 更新 Anaconda base 虚拟环境" -ForegroundColor Magenta -BackgroundColor Cyan
+	conda activate base
+	conda upgrade python
+	
+	# update TeX Live
+	$CurrentYear = Get-Date -Format yyyy
+	Write-Host "Step 2: 更新 TeX Live" $CurrentYear -ForegroundColor Magenta -BackgroundColor Cyan
+	tlmgr update --self
+	tlmgr update --all
+	
+	# update Chocolotey
+	# Write-Host "Step 3: 更新 Chocolatey" -ForegroundColor Magenta -BackgroundColor Cyan
+	# choco outdated
+	
+	# update Apps using winget
+	Write-Host "Step 3: 通过 winget 更新 Windows 应用程序" -ForegroundColor Magenta -BackgroundColor Cyan
+	winget upgrade
+	winget upgrade --all
+	
 }
 
+Function aliasEFunc {Search-Everything -PathExclude 'C:\users\Crbk01\AppData\Local\Temp'-Filter '<wholefilename:child:.git file:>|<wholefilename:child:.git folder:>' -global | #todo: rename #todo: breakout
+Where{ $_ -notmatch 
+'C..9dfe73ef|OneDrive|GitHubDesktop.app|Microsoft VS Code._.resources.app|Installer.resources.app.node_modules|Microsoft.E dge.User Data.*.Extensions|Program Files.*.(Esri|MapInfo|ArcGIS)|Recycle.Bin' 
+}}  ;
+
 #todo: breakout
-function pstree {
-	# https://gist.github.com/aroben/5542538
-	$ProcessesById = @{}
-	foreach ($Process in (Get-WMIObject -Class Win32_Process)) {
-		$ProcessesById[$Process.ProcessId] = $Process
+function Show-ProcessTree{
+	param([UInt32]$ProcessId, $IndentLevel) 
+	$Process = $ProcessesById[$ProcessId]
+	$Indent = " " * $IndentLevel
+	if ($Process.CommandLine) {
+		$Description = $Process.CommandLine
+	} else {
+		$Description = $Process.Caption
 	}
 
-	$ProcessesWithoutParents = @()
-	$ProcessesByParent = @{}
-	foreach ($Pair in $ProcessesById.GetEnumerator()) {
-		$Process = $Pair.Value
+	Write-Output ("{0,6}{1} {2}" -f $Process.ProcessId, $Indent, $Description)
 
-		if (($Process.ParentProcessId -eq 0) -or !$ProcessesById.ContainsKey($Process.ParentProcessId)) {
-			$ProcessesWithoutParents += $Process
-			continue
-		}
-
-		if (!$ProcessesByParent.ContainsKey($Process.ParentProcessId)) {
-			$ProcessesByParent[$Process.ParentProcessId] = @()
-		}
-		$Siblings = $ProcessesByParent[$Process.ParentProcessId]
-		$Siblings += $Process
-		$ProcessesByParent[$Process.ParentProcessId] = $Siblings
-	}
-
-	#todo: breakout
-	function Show-ProcessTree([UInt32]$ProcessId, $IndentLevel) {
-		$Process = $ProcessesById[$ProcessId]
-		$Indent = " " * $IndentLevel
-		if ($Process.CommandLine) {
-			$Description = $Process.CommandLine
-		} else {
-			$Description = $Process.Caption
-		}
-
-		Write-Output ("{0,6}{1} {2}" -f $Process.ProcessId, $Indent, $Description)
-		foreach ($Child in ($ProcessesByParent[$ProcessId] | Sort-Object CreationDate)) {
-			Show-ProcessTree $Child.ProcessId ($IndentLevel + 4)
-		}
-	}
-
+	foreach ($Child in ($ProcessesByParent[$ProcessId] | Sort-Object CreationDate)) {
+		Show-ProcessTree $Child.ProcessId ($IndentLevel + 4) }
+	
 	Write-Output ("{0,6} {1}" -f "PID", "Command Line")
 	Write-Output ("{0,6} {1}" -f "---", "------------")
 
@@ -99,41 +122,26 @@ function pstree {
 	}
 }
 
-#todo: breakout
-function Update-Packages {
-	# Python 直接执行
-	#$env:PATHEXT += ";.py"
-	# 更新系统组件
-	# update pip
-	# Write-Host "Step 1: 更新 pip" -ForegroundColor Magenta -BackgroundColor Cyan
-	# $a = pip list --outdated
-	# $num_package = $a.Length - 2
-	# for ($i = 0; $i -lt $num_package; $i++) {
-	# 	$tmp = ($a[2 + $i].Split(" "))[0]
-	# 	pip install -U $tmp
-	# }
-
-	# update conda
-	Write-Host "Step 1: 更新 Anaconda base 虚拟环境" -ForegroundColor Magenta -BackgroundColor Cyan
-	conda activate base
-	conda upgrade python
-
-	# update TeX Live
-	$CurrentYear = Get-Date -Format yyyy
-	Write-Host "Step 2: 更新 TeX Live" $CurrentYear -ForegroundColor Magenta -BackgroundColor Cyan
-	tlmgr update --self
-	tlmgr update --all
-
-	# update Chocolotey
-	# Write-Host "Step 3: 更新 Chocolatey" -ForegroundColor Magenta -BackgroundColor Cyan
-	# choco outdated
-
-	# update Apps using winget
-	Write-Host "Step 3: 通过 winget 更新 Windows 应用程序" -ForegroundColor Magenta -BackgroundColor Cyan
-	winget upgrade
-	winget upgrade --all
-
+function unzip{
+ 	param($file)  #todo: breakou
+	$dirname = (Get-Item $file).Basename
+	echo("Extracting", $file, "to", $dirname)
+	New-Item -Force -ItemType directory -Path $dirname
+	expand-archive $file -OutputPath $dirname -ShowProgress
 }
+
+	
+# todo: breake out to file
+function sed-recursive{
+	param($filePattern, $find, $replace) 
+	$files = ls . "$filePattern" -rec
+	foreach ($file in $files) {
+		(Get-Content $file.PSPath) |
+		Foreach-Object { $_ -replace "$find", "$replace" } |
+		Set-Content $file.PSPath
+	}
+}
+
 
 #del alias:gc -Force
 #del alias:gp -Force
@@ -209,6 +217,10 @@ function uptimef { Get-WmiObject win32_operatingsystem | select csname, @{LABEL=
 
 function which($name) { Get-Command $name | Select-Object -ExpandProperty Definition } #should use more
 
+function aliasCode { & $env:code }
+
+function aliasfifs { get-childitem | out-string -stream }
+
 #-------------------------------    Functions END     -------------------------------
 
 
@@ -219,8 +231,8 @@ function which($name) { Get-Command $name | Select-Object -ExpandProperty Defini
 #set-alias -Name gp -Value pull
 set-alias -Name lsx -Value aliasListDirectory
 
-set-alias -name code -value '& $env:code'
-set-alias -Name filesinfolasstream -Value 'get-childitem | out-string -stream'
+set-alias -name code -value aliasCode
+set-alias -Name filesinfolasstream -Value aliasfifs
 set-alias -Name bcompare -Value aliasbc
 
 set-alias -name EveryGitRepo -Value aliasEFunc
