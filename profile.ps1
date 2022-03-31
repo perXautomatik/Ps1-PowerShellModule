62ca77141a9d6e4afc2f128ed3c17ab238d7c341
perXautomatik <christoffer.broback@gmail.com>
2022-03-31 12:02:12 +0200

    [chore] one liners identified [feature] added todo and notes

--- profile.ps1
+++ profile.ps1
@@ -4,53 +4,27 @@
 
 
 # 1. 编译函数 make
-function aliasMakeThings {
-	nmake.exe $args -nologo
-};
+function aliasMakeThings { nmake.exe $args -nologo };
 
-
-# 2. 更新系统 os-update
 
 # 3. 查看目录 ls & ll
-function aliasListDirectory {
-	(Get-ChildItem).Name
-	Write-Host("")
-};
+function aliasListDirectory { (Get-ChildItem).Name ; Write-Host("") };
+
+# 4. 打开当前工作目录 # 输入要打开的路径 # 用法示例：open C:\ # 默认路径：当前工作文件夹
+function aliasOpenCurrentFolderF { param ( $Path = '.' ) Invoke-Item $Path };
 
 
-# 4. 打开当前工作目录
-function aliasOpenCurrentFolderF {
-	param
-	(
-		# 输入要打开的路径
-		# 用法示例：open C:\
-		# 默认路径：当前工作文件夹
-		$Path = '.'
-	)
-	Invoke-Item $Path
-};
 
+#if no param, navigate to desktop # 5. 更改工作目录 # 输入要切换到的路径 # 用法示例：cd C:/ # 默认路径：D 盘的桌面
+function aliasChangeDirectory { param ( $Path = '%USERPROFILE%\Desktop\' ) Set-Location $Path };
 
-# 5. 更改工作目录
-function aliasChangeDirectory {
-	param (
-		# 输入要切换到的路径
-		# 用法示例：cd C:/
-		# 默认路径：D 盘的桌面
-		$Path = 'D:\Users\newton\Desktop\'
-	)
-	Set-Location $Path
-};
+#doesn't psreadline module implement this already?
+function uptimef { Get-WmiObject win32_operatingsystem | select csname, @{LABEL='LastBootUpTime'; EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}} }
 
-function uptime {
-	Get-WmiObject win32_operatingsystem | select csname, @{LABEL='LastBootUpTime';
-	EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}
-}
+#reload-profile is an unapproved verb.
+function aliasreloadprofile { & $profile }
 
-function reload-profile {
-	& $profile
-}
-
+#todo: breakout
 function find-file($name) {
 	ls -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | foreach {
 		$place_path = $_.directory
@@ -58,10 +32,9 @@ function find-file($name) {
 	}
 }
 
-function print-path {
-	($Env:Path).Split(";")
-}
+function print-path { ($Env:Path).Split(";") }
 
+#todo: breakout
 function unzip ($file) {
 	$dirname = (Get-Item $file).Basename
 	echo("Extracting", $file, "to", $dirname)
@@ -71,25 +44,29 @@ function unzip ($file) {
 
 
 # filesInFolAsStream ;
-function aliasfilesInFolAsStream {
-	get-childitem | out-string -stream
-}
+function aliasfilesInFolAsStream { get-childitem | out-string -stream }
 
 
 #new ps OpenAsADmin
-function aliasopenasadminf {
-	Start-Process powershell -Verb runAs
-}
+function aliasopenasadminf { Start-Process powershell -Verb runAs }
 
 
-Function aliasEFunc {Search-Everything -PathExclude 'C:\users\Crbk01\AppData\Local\Temp'-Filter '<wholefilename:child:.git file:>|<wholefilename:child:.git folder:>' -global | Where{ $_ -notmatch 'C..9dfe73ef|OneDrive|GitHubDesktop.app|Microsoft VS Code._.resources.app|Installer.resources.app.node_modules|Microsoft.E dge.User Data.*.Extensions|Program Files.*.(Esri|MapInfo|ArcGIS)|Recycle.Bin' }}  ;
+#todo: breakout
+#todo: rename
+Function aliasEFunc {Search-Everything -PathExclude 'C:\users\Crbk01\AppData\Local\Temp'-Filter '<wholefilename:child:.git file:>|<wholefilename:child:.git folder:>' -global |
+ Where{ $_ -notmatch 
+	'C..9dfe73ef|OneDrive|GitHubDesktop.app|Microsoft VS Code._.resources.app|Installer.resources.app.node_modules|Microsoft.E dge.User Data.*.Extensions|Program Files.*.(Esri|MapInfo|ArcGIS)|Recycle.Bin' 
+}}  ;
 
 
-
+#todo: breakout
+#todo: rename to more descriptive
+#todo: parameterize
 Function aliasEGSfunc {cd $_; Out-File -FilePath .\lazy.log -inputObject (git lazy 'AutoCommit' 2>&1 )} ;
 
 
 
+#todo: breakout
 Function aliasEGSRfunc
 {
 	out-null -InputObject( git remote -v | Tee-Object -Variable proc ) ;
@@ -101,41 +78,35 @@ Function aliasEGSRfunc
 
 
 
-function aliasFunctionEverything([string]$filter)
-	{Search-Everything -filter $filter -global}
+function aliasFunctionEverything([string]$filter) {Search-Everything -filter $filter -global}
 
-function aliasPshellHistoryPath {
-	(Get-PSReadlineOption).HistorySavePath
-}
+function aliasPshellHistoryPath { (Get-PSReadlineOption).HistorySavePath }
 
-function aliasPastDo($searchstring) {
-$path = aliasPshellHistoryPath; menu @( get-content $path | where{ $_ -match $searchstring }) | %{Invoke-Expression $_ }
-}
+#search history of past expressions and invokes it, doesn't register the expression itself in history, but the pastDo expression.
+function aliasPastDo($searchstring) { $path = aliasPshellHistoryPath; menu @( get-content $path | where{ $_ -match $searchstring }) | %{Invoke-Expression $_ } }
 
-function aliasPastDoEdit($searchstring) {
-$path = aliasPshellHistoryPath; menu @( get-content $path | where{ $_ -match $searchstring }) | %{ Set-Clipboard -Value $_ }
-}
+#search history of past expressions and adds to clipboard
+function aliasPastDoEdit($searchstring) { $path = aliasPshellHistoryPath; menu @( get-content $path | where{ $_ -match $searchstring }) | %{ Set-Clipboard -Value $_ }}
 
 
+#use everything to find executable for fast execution
+#todo: sort by rescent use
+#use whatpulse db first, then everything
 function aliasExecuteThis($searchstring) {
 menu @(everything "ext:exe $searchString") | %{& $_ }
 }
 
 
-function aliasMyAliases {
-Get-Alias -Definition alias* | select name
-}
+function aliasMyAliases { Get-Alias -Definition alias* | select name }
 
 
 
 
 #Git Ad $leaf as submodule from $remote and branch $branch
-Function aliasEFuncGT([string]$leaf,[string]$remote,[string]$branch)
-{
- git submodule add -f --name $leaf -- $remote $branch ; git commit -am $leaf+$remote+$branch
- } ;
+#todo: move to git aliases
+Function aliasEFuncGT([string]$leaf,[string]$remote,[string]$branch) { git submodule add -f --name $leaf -- $remote $branch ; git commit -am $leaf+$remote+$branch } ;
 
-
+#todo: move to git aliases
 Function aliasEGLp($path,$message) { cd $path ; git add .; git commit -m $message ; git push } ;
 
 
@@ -145,30 +116,23 @@ function checkout () { & git checkout $args }
 #del alias:gc -Force
 #del alias:gp -Force
 
-function aliasbc($REMOTE,$LOCAL,$BASE,$MERGED) {
-cmd /c "C:\Users\crbk01\Desktop\WhenOffline\BeondCompare4\BComp.exe" "$REMOTE" "$LOCAL" "$BASE" "$MERGED"
-}
+#todo: replace hardcode with global variable pointing to path
+function aliasbc($REMOTE,$LOCAL,$BASE,$MERGED) { cmd /c "C:\Users\crbk01\Desktop\WhenOffline\BeondCompare4\BComp.exe" "$REMOTE" "$LOCAL" "$BASE" "$MERGED" }
 
-function aliasviv {
-vivaldi "vivaldi://flags"
-}
+#todo: use standard browser instead of hardcoded
+function aliasviv { vivaldi "vivaldi://flags" }
 
-
-function aliasrb {
-shutdown /r
-}
+#reboot
+function aliasrb { shutdown /r }
 
 # Unixlike commands
 #######################################################
 
-function df {
-	get-volume
-}
+function df { get-volume }
 
-function sed($file, $find, $replace){
-	(Get-Content $file).replace("$find", $replace) | Set-Content $file
-}
+function sed($file, $find, $replace){ (Get-Content $file).replace("$find", $replace) | Set-Content $file }
 
+# todo: breake out to file
 function sed-recursive($filePattern, $find, $replace) {
 	$files = ls . "$filePattern" -rec
 	foreach ($file in $files) {
@@ -178,38 +142,22 @@ function sed-recursive($filePattern, $find, $replace) {
 	}
 }
 
-function grep($regex, $dir) {
-	if ( $dir ) {
-		ls $dir | select-string $regex
-		return
-	}
-	$input | select-string $regex
-}
+function grep($regex, $dir) { if ( $dir ) { ls $dir | select-string $regex return } $input | select-string $regex }
 
-function grepv($regex) {
-	$input | ? { !$_.Contains($regex) }
-}
+function grepv($regex) { $input | ? { !$_.Contains($regex) } }
 
-function which($name) {
-	Get-Command $name | Select-Object -ExpandProperty Definition
-}
+#should use more
+function which($name) { Get-Command $name | Select-Object -ExpandProperty Definition }
 
-function export($name, $value) {
-	set-item -force -path "env:$name" -value $value;
-}
+function export($name, $value) { set-item -force -path "env:$name" -value $value; }
 
-function pkill($name) {
-	ps $name -ErrorAction SilentlyContinue | kill
-}
+function pkill($name) { ps $name -ErrorAction SilentlyContinue | kill }
 
-function pgrep($name) {
-	ps $name
-}
+function pgrep($name) { ps $name }
 
-function touch($file) {
-	"" | Out-File $file -Encoding ASCII
-}
+function touch($file) { "" | Out-File $file -Encoding ASCII }
 
+#todo: breakout
 function sudo {
 	$file, [string]$arguments = $args;
 	$psi = new-object System.Diagnostics.ProcessStartInfo $file;
@@ -219,7 +167,7 @@ function sudo {
 	[System.Diagnostics.Process]::Start($psi) >> $null
 }
 
-
+#todo: breakout
 function pstree {
 	# https://gist.github.com/aroben/5542538
 	$ProcessesById = @{}
@@ -245,6 +193,7 @@ function pstree {
 		$ProcessesByParent[$Process.ParentProcessId] = $Siblings
 	}
 
+	#todo: breakout
 	function Show-ProcessTree([UInt32]$ProcessId, $IndentLevel) {
 		$Process = $ProcessesById[$ProcessId]
 		$Indent = " " * $IndentLevel
@@ -268,11 +217,12 @@ function pstree {
 	}
 }
 
-# Python 直接执行
-#$env:PATHEXT += ";.py"
 
-# 更新系统组件
+#todo: breakout
 function Update-Packages {
+	# Python 直接执行
+	#$env:PATHEXT += ";.py"
+	# 更新系统组件
 	# update pip
 	# Write-Host "Step 1: 更新 pip" -ForegroundColor Magenta -BackgroundColor Cyan
 	# $a = pip list --outdated
@@ -307,21 +257,15 @@ function Update-Packages {
 
 #-------------------------------   Set Network BEGIN    -------------------------------
 # 1. 获取所有 Network Interface
-function Get-AllNic {
-	Get-NetAdapter | Sort-Object -Property MacAddress
-}
+function Get-AllNic { Get-NetAdapter | Sort-Object -Property MacAddress }
 
 
 # 2. 获取 IPv4 关键路由
-function Get-IPv4Routes {
-	Get-NetRoute -AddressFamily IPv4 | Where-Object -FilterScript {$_.NextHop -ne '0.0.0.0'}
-}
+function Get-IPv4Routes { Get-NetRoute -AddressFamily IPv4 | Where-Object -FilterScript {$_.NextHop -ne '0.0.0.0'} }
 
 
 # 3. 获取 IPv6 关键路由
-function Get-IPv6Routes {
-	Get-NetRoute -AddressFamily IPv6 | Where-Object -FilterScript {$_.NextHop -ne '::'}
-}
+function Get-IPv6Routes { Get-NetRoute -AddressFamily IPv6 | Where-Object -FilterScript {$_.NextHop -ne '::'} }
 
 #-------------------------------    Set Network END     -------------------------------
 
@@ -329,49 +273,45 @@ function Get-IPv6Routes {
 
 
 #-------------------------------   Set alias BEGIN    -------------------------------
-set-alias -name reload-profile -value reloadProfile
-set-alias -name uptime -value uptimef
-set-alias -name print-path -value printpath
 
-set-alias -name unzip -value unzipf    
-
-set-alias -Name getip6 -Value Get-IPv6Routes      
-set-alias -Name getip -Value Get-IPv4Routes
-set-alias -Name getnic -Value Get-AllNic
-set-alias -Name reboot -Value aliasrb
-set-alias -Name browserflags -Value aliasviv
-set-alias -Name bcompare -Value aliasbc
-
+#set-alias -Name cd -Value aliasChangeDirectory -Option AllScope
 #set-alias -Name gc -Value checkout
 #set-alias -Name gp -Value pull
-set-alias -name GitUp -value aliasEGLp
-set-alias -name GitAdEPathAsSNB -value aliasEFuncGT
-
-set-alias -name MyAliases -value aliasMyAliases
-
-set-alias -name executeThis -value aliasExecuteThis
-
-set-alias -name pastDoEdit -value aliasPastDoEdit
-
-set-alias -name pastDo -value aliasPastDo
-set-alias -name pshelHistorypath -value aliasPshellHistoryPath
+#set-alias -Name ls -Value aliasListDirectory
 
 set-alias -name code -value '& $env:code'
-set-alias -name gitSingleRemote -Value aliasEGSRfunc
-set-alias -name gitSilently -Value aliasEGSfunc
+set-alias -Name bcompare -Value aliasbc
+
 set-alias -name EveryGitRepo -Value aliasEFunc
 
-set-alias -Name OpenAsADmin -Value aliasopenasadminf
-set-alias -Name filesinfolasstream -Value aliasfilesInFolAsStream
-#set-alias -Name cd -Value aliasChangeDirectory -Option AllScope
+set-alias -name GitAdEPathAsSNB -value aliasEFuncGT
+set-alias -name GitUp -value aliasEGLp
+set-alias -name gitSilently -Value aliasEGSfunc
+set-alias -name gitSingleRemote -Value aliasEGSRfunc
 
-set-alias -Name open-current-folder -Value aliasOpenCurrentFolderF
-#set-alias -Name ls -Value aliasListDirectory
-set-alias -Name ll -Value Get-ChildItem            
+set-alias -name executeThis -value aliasExecuteThis
+set-alias -Name filesinfolasstream -Value aliasfilesInFolAsStream
+set-alias -name everything -value aliasFunctionEverything
 set-alias -Name make -Value aliasMakeThings
+set-alias -name MyAliases -value aliasMyAliases
+set-alias -Name OpenAsADmin -Value aliasopenasadminf
+set-alias -Name open-current-folder -Value aliasOpenCurrentFolderF
+set-alias -name pastDo -value aliasPastDo
+set-alias -name pastDoEdit -value aliasPastDoEdit
+set-alias -name pshelHistorypath -value aliasPshellHistoryPath
+set-alias -Name reboot -Value aliasrb
+set-alias -Name browserflags -Value aliasviv
+set-alias -name print-path -value printpath
+set-alias -name reload-profile -value reloadProfile
+set-alias -name unzip -value unzipf    
+set-alias -name uptime -value uptimef
+
+set-alias -Name getnic -Value Get-AllNic
+set-alias -Name ll -Value Get-ChildItem            
+set-alias -Name getip -Value Get-IPv4Routes
+set-alias -Name getip6 -Value Get-IPv6Routes      
 set-alias -Name os-update -Value Update-Packages
 
-set-alias -name everything -value aliasFunctionEverything
 
 echo "Alias set"
 #-------------------------------    Set alias END     -------------------------------
