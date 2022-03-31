024a961257c17c613c58fe3590c28621d1dd6119
perXautomatik <christoffer.broback@gmail.com>
2022-03-31 21:36:43 +0200

    [chore] merged in a few more snips

--- ToMerge/Delete_Days_Back.ps1
+++
@@ -1,7 +0,0 @@
-## Variables
-$Dest = "C:\Support\SQLBac\";    # Backup path on server (optional).
-$Daysback = "0";                 # Days to keep.
-
-$CurrentDate = Get-Date
-$DatetoDelete = $CurrentDate.AddDays($Daysback)
-Get-ChildItem $Dest | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item
